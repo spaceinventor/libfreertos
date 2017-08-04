4bf436e45e02bf246a7e8800a45ec936a4794f4a
Johan De Claville Christiansen <johan@safeex.com>
2017-08-04 13:25:10 +0200

    Added run-time initialization of freertos clocks

---
+++ src/freertos_init.c
@@ -0,0 +1,16 @@
+/*
+ * freertos_init.c
+ *
+ *  Created on: Aug 4, 2017
+ *      Author: johan
+ */
+
+#include <FreeRTOS.h>
+
+unsigned int freertos_clock_hz = 0;
+TickType_t freertos_tick_rate_hz = 0;
+
+void freertos_set_clocks(unsigned int clock_hz, tick_rate_hz) {
+	freertos_clock_hz = clock_hz;
+	freertos_tick_rate_hz = tick_rate_hz;
+}
